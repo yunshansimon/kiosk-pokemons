8021b0efbbbebf779ec85bf19a5e53477ff1475f
Yang Tsao <yang@flintos.io>
2018-11-30 20:45:05 +0800

    full project

---
+++ config.js
@@ -0,0 +1,9 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "Pokemons wiki",
+   "homepage": "https://www.pokedex.org",
+   "sessionDataTimeoutTime": 5,
+   "sessionTimeoutTime": 5,
+   "kioskEnabled": true
+};
